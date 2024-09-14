68729ece077c90e03161ca1f6ad7813fb75e1b4c
JTPO <jorosepooo@gmail.com>
2024-09-14 20:35:56 +0800

    Update something number

--- Assets/Scripts/SceneData.cs
+++ Assets/Scripts/SceneData.cs
@@ -6,12 +6,11 @@ public class SceneData : MonoBehaviour
 {
     public static int home = 0;
     public static int load = 1;
-    public static int setting = 2;
-    public static int eggselect = 3;
-    public static int egghatchcat = 4;
-    public static int egghatchdog = 5;
-    public static int maincat = 6;
-    public static int maindog = 7;
+    public static int eggselect = 2;
+    public static int egghatchcat = 3;
+    public static int egghatchdog = 4;
+    public static int maincat = 5;
+    public static int maindog = 6;
 
 
 }
