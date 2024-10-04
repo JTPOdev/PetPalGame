ef24965207df166aa4b86e935ae0c7064667c3b3
Kristine <krdu.castres.up@phinmaed.com>
2024-10-04 21:54:51 +0800

    updated

--- Assets/Scripts/NameScript.cs
+++ Assets/Scripts/NameScript.cs
@@ -1,55 +1,55 @@
-using System.Collections;
-using TMPro;
-using UnityEngine;
-using UnityEngine.Networking;
+// using System.Collections;
+// using TMPro;
+// using UnityEngine;
+// using UnityEngine.Networking;
 
-public class NameScript : MonoBehaviour
-{
-    public TextMeshProUGUI display_pet_name;
+// public class NameScript : MonoBehaviour
+// {
+//     public TextMeshProUGUI display_pet_name;
 
-    public void Awake()
-    {
-         display_pet_name.text = PawScript.scene1.pet_name; 
-    }
+//     public void Awake()
+//     {
+//          display_pet_name.text = PawScript.scene1.pet_name; 
+//     }
 
-    void Start()
-    {
-        StartCoroutine(GetPetNames());
-    }
+//     void Start()
+//     {
+//         StartCoroutine(GetPetNames());
+//     }
 
-    IEnumerator GetPetNames()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/PetPalGame/GetName.php"))
-        {
-            yield return www.SendWebRequest();
+//     IEnumerator GetPetNames()
+//     {
+//         using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/PetPalGame/GetName.php"))
+//         {
+//             yield return www.SendWebRequest();
 
-            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
-            {
-                Debug.LogError("Error: " + www.error);
-            }
-            else
-            {
-                // JSON response
-                string jsonResponse = www.downloadHandler.text;
-                NameList petNamesList = JsonUtility.FromJson<NameList>(jsonResponse);
+//             if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+//             {
+//                 Debug.LogError("Error: " + www.error);
+//             }
+//             else
+//             {
+//                 // JSON response
+//                 string jsonResponse = www.downloadHandler.text;
+//                 NameList petNamesList = JsonUtility.FromJson<NameList>(jsonResponse);
 
 
-                if (petNamesList.names.Length > 0)
-                {
-                    display_pet_name.text = petNamesList.names[20]; 
-                }
-                else
-                {
-                    display_pet_name.text = "No names found.";
-                }
-            }
-        }
-    }
-}
+//                 if (petNamesList.names.Length > 0)
+//                 {
+//                     display_pet_name.text = petNamesList.names[20]; 
+//                 }
+//                 else
+//                 {
+//                     display_pet_name.text = "No names found.";
+//                 }
+//             }
+//         }
+//     }
+// }
 
-// Create the NameList class to hold the JSON response
-[System.Serializable]
-public class NameList
-{
-    public string[] names;
-}
+// // Create the NameList class to hold the JSON response
+// [System.Serializable]
+// public class NameList
+// {
+//     public string[] names;
+// }
--- Assets/Scripts/PawScript.cs
+++ Assets/Scripts/PawScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEngine.Networking;
+// using UnityEngine.Networking;
 
 public class PawScript : MonoBehaviour
 {
@@ -50,7 +50,7 @@ public class PawScript : MonoBehaviour
 
     public void openEggCat()
     {
-        StartCoroutine(SubmitPetName(""));
+        // StartCoroutine(SubmitPetName(""));
         AudioManager.instance.Play("ButtonPressed");
         PawAnim.gameObject.SetActive(true);
         LeanTween.scale(PawAnim, Vector3.zero, 0f);
@@ -64,39 +64,39 @@ public class PawScript : MonoBehaviour
 
     public void openEggDog()
     {
-        StartCoroutine(SubmitPetName(""));
+        // StartCoroutine(SubmitPetName(""));
         AudioManager.instance.Play("ButtonPressed");
         PawAnim.gameObject.SetActive(true);
         LeanTween.scale(PawAnim, Vector3.zero, 0f);
         LeanTween.scale(PawAnim, new Vector3(1, 1, 1), 1.1f).setEase(LeanTweenType.easeInOutExpo).setOnComplete(() =>
         {
-            pet_name = inputField.text;
+            // pet_name = inputField.text;
             PlayerPrefs.SetString("PetName", pet_name); 
             SceneManager.LoadScene(SceneData.egghatchdog);
         });
     }
-    private IEnumerator SubmitPetName(string type)
-    {
-        pet_name = inputField.text; // Get the name from the input field
-        PlayerPrefs.SetString("PetName", pet_name); // Save the pet name
+    // private IEnumerator SubmitPetName(string type)
+    // {
+    //     pet_name = inputField.text; // Get the name from the input field
+    //     PlayerPrefs.SetString("PetName", pet_name); // Save the pet name
 
-        // Create form data
-        WWWForm form = new WWWForm();
-        form.AddField("name", pet_name); // Add name to form data
+    //     // Create form data
+    //     WWWForm form = new WWWForm();
+    //     form.AddField("name", pet_name); // Add name to form data
 
-        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/PetPalGame/GetName.php", form))
-        {
-            yield return www.SendWebRequest();
+    //     using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/PetPalGame/GetName.php", form))
+    //     {
+    //         yield return www.SendWebRequest();
 
-            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
-            {
-                Debug.LogError("Error: " + www.error);
-            }
-            else
-            {
+    //         if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+    //         {
+    //             Debug.LogError("Error: " + www.error);
+    //         }
+    //         else
+    //         {
                 
-                SceneManager.LoadScene(type == "cat" ? SceneData.egghatchcat : SceneData.egghatchdog);
-            }
-        }
-    }
+    //             SceneManager.LoadScene(type == "cat" ? SceneData.egghatchcat : SceneData.egghatchdog);
+    //         }
+    //     }
+    // }
 }
